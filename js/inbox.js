64bb011123326c7ec1beaabca1a69a049511b272
Patrick Boguet <p.boguet@hotmail.fr>
2015-03-20 11:47:55 +0100

    ajoutClic

--- js/inbox.js
+++ js/inbox.js
@@ -1,6 +1,9 @@
 $('.fab').hover(function () {
     $(this).toggleClass('active');
 });
+$('.fab').click(function () {
+    $(this).toggleClass('active');
+});
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
