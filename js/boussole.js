93155ccf3209937401598726fa7e17e62da059ef
Patrick Boguet <p.boguet@hotmail.fr>
2015-03-20 15:58:33 +0100

    boussole

--- js/boussole.js
+++ js/boussole.js
@@ -1,10 +1,35 @@
-$(document).addEventListener('deviceready',function(){
+$(document).ready(function(){
+	document.addEventListener("deviceready", onDeviceReady, false);
+});
 
-	var watchID = navigator.compass.watchHeading(
-		function(direction){
-			$('#disk').css('transform', 'rotate('+direction.trueHeading+'deg)');
-		},
-		function(){
-			alert('erreur !');
-		});
-},false);
+
+
+
+// device APIs are available
+//
+function onDeviceReady() {
+
+	var compassSuccess=function(heading) {
+		//$('#debug').html(heading.magneticHeading);
+		$('#disk').css('transform','rotate('+(-1*parseInt(heading.magneticHeading))+'deg)');
+		
+		setTimeout(getCompass,250);
+		
+		return;
+	};
+	var compassError=function(compassError) {
+		setTimeout(getCompass,250);
+		//alert('Compass error: ' + compassError.code);
+		return;
+	};
+
+
+
+	function getCompass()
+	{
+		if(navigator.compass) {
+			navigator.compass.getCurrentHeading(compassSuccess, compassError);
+		}
+	}
+	getCompass();
+}
