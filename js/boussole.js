54b2ba09b9751ce1500f58a79121256649a55e2e
Patrick Boguet <p.boguet@hotmail.fr>
2015-03-20 13:53:47 +0100

    trueHeading

--- js/boussole.js
+++ js/boussole.js
@@ -2,7 +2,7 @@ $(document).addEventListener('deviceready',function(){
 
 	var watchID = navigator.compass.watchHeading(
 		function(direction){
-			$('#disk').css('transform', 'rotate('+direction+'deg)');
+			$('#disk').css('transform', 'rotate('+direction.trueHeading+'deg)');
 		},
 		function(){
 			alert('erreur !');
